865d6cf3f25237733c6475591f3600179763266f
Nithin Mohan <nithinmohan094@gmail.com>
2014-12-21 18:26:10 +0530

    added chart showing js

--- js/script.js
+++ js/script.js
@@ -54,4 +54,9 @@ var top_array=[];
 		}
 	})
 	calculate_top_array();
-	move_to_slide(0);
+	move_to_slide(4);
+	$(".chart_container").hide();
+	$(".chart_link").click(function(){
+		$(".chart_container").fadeOut();
+		$("#"+$(this).data("chart-target")).slideDown();
+	});
